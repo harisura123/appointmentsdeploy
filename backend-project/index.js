1bf4461df1de39964601b4f129fb19c7cae763cd
Harisurya <surya7075303536@gmail.com>
2025-02-26 07:25:23 +0530

    Update index.js

--- backend-project/index.js
+++ backend-project/index.js
@@ -6,9 +6,13 @@ const MongooseAddDoctor = require('./data/AddDoctor')
 const MongooseAddPatient = require('./patient/PatientDetails')
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: [],
+    methods: ["GET","POST", "PUT", "DELETE"],
+    credentials: true
+}))
 
-mongoose.connect('mongodb://127.0.0.1:27017/appointment')
+mongoose.connect('mongodb+srv://surya_user:surya123@mern2025.rrmsu.mongodb.net/?retryWrites=true&w=majority&appName=mern2025')
 
 app.listen(3002, () => {
     console.log('server is running')
